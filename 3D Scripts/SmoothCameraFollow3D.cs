4a3e495aaba37443a744b6951d8079825d25b53d
Rijk van Putten <rijkvanputten@hotmail.nl>
2019-10-15 17:51:56 +0200

    Added SmoothCameraFollow3D

---
+++ 3D Scripts/SmoothCameraFollow3D.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class SmoothCameraFollow3D : MonoBehaviour
+{
+    public Transform target;
+    public enum CameraFollowUpdateMode { Update, FixedUpdate, LateUpdate }
+    public CameraFollowUpdateMode updateMode = CameraFollowUpdateMode.FixedUpdate;
+    public float smoothSpeed = 0.1f;
+    public Vector3 offset;
+
+    private void Start()
+    {
+       
+    }
+
+    void FixedUpdate()
+    {
+        if (updateMode == CameraFollowUpdateMode.FixedUpdate)
+            Follow();
+    }
+
+    private void Update()
+    {
+        if (updateMode == CameraFollowUpdateMode.Update)
+            Follow();
+    }
+
+    private void LateUpdate()
+    {
+        if (updateMode == CameraFollowUpdateMode.LateUpdate)
+            Follow();
+    }
+
+    void Follow()
+    {
+        if (target)
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
