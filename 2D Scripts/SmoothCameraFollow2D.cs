efdd617f7b93da54dcdec3a51fdfa7ac48d0df3c
Rijk-van-Putten <rijkvanputten@hotmail.nl>
2020-05-16 17:11:01 +0200

    Update SmoothCameraFollow2D.cs

--- 2D Scripts/SmoothCameraFollow2D.cs
+++ 2D Scripts/SmoothCameraFollow2D.cs
@@ -7,12 +7,14 @@ public class SmoothCameraFollow2D : MonoBehaviour
     public CameraFollowUpdateMode updateMode = CameraFollowUpdateMode.FixedUpdate;
     public float smoothSpeed = 0.1f;
     public float zPosition = -100;
+    public bool followX = true;
+    public bool followY = true;
     public Vector2 offset;
 
     [Header("Size with Velocity (optional)")]
     public bool sizeWithVelocity = false;
     public float defaultSize = 5.0f;
-    public float velocityMultiplier = 1.0f;
+    public Vector2 velocityMultiplier = Vector2.one;
     public new Camera camera;
     public new Rigidbody2D rigidbody;
 
@@ -43,14 +45,16 @@ public class SmoothCameraFollow2D : MonoBehaviour
     {
         if (target)
         {
-            Vector2 desiredPosition = new Vector2(target.position.x, target.position.y) + offset;
+            Vector2 desiredPosition = new Vector2(followX ? target.position.x : 0, followY ? target.position.y : 0) + offset;
             Vector2 smoothedPosition = Vector2.Lerp(transform.position, desiredPosition, smoothSpeed);
             transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, zPosition);
 
             if (sizeWithVelocity)
             {
-                float velocity = rigidbody.velocity.magnitude;
-                camera.orthographicSize = defaultSize + velocity * velocityMultiplier;
+                float velocity = (rigidbody.velocity * velocityMultiplier).magnitude;
+                float targetSize = defaultSize + velocity;
+                float smoothSize = Mathf.Lerp(camera.orthographicSize, targetSize, Time.deltaTime);
+                camera.orthographicSize = smoothSize;
             }
         }
     }
